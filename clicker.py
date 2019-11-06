da7abb0e7cc5cae0b3c8b8627f23a583e19c8578
sharjani <sharjani@umich.edu>
2019-11-06 18:56:16 -0500

    Added in a more dynamic interface and a refresh feature. Also, will click a second after the office hour queue opens.

--- clicker.py
+++ clicker.py
@@ -2,23 +2,33 @@ import os
 import pyautogui, sys
 import datetime
 import keyboard
+import time
+from pynput.keyboard import Key, Controller
 
 print('Press Ctrl-C to quit.')
+hours, minutes = input("enter the time Office Hours opens up (Hours(24-hr format) Minutes)").split()
+hours = int(hours)
+minutes = int(minutes)
 
+#print(type(hours))
 print("Hover mouse over 'get help' button and press A")
+my_keyboard = Controller()
 while not keyboard.is_pressed('a'):
-    print(" ")
+    pass
 my_position = pyautogui.position()
 try:
     pyautogui.moveTo(my_position)
     while 1:
         time = datetime.datetime.now()
-        if datetime.datetime.now().minute == 1:
-            if datetime.datetime.now().second > 1:
+        if datetime.datetime.now().minute == minutes:
+            if datetime.datetime.now().second >= 0:
                 break
-        print(datetime.datetime.now())
+        #print(datetime.datetime.now())
         if keyboard.is_pressed('q'):
             break
+    pyautogui.moveTo(my_position)
+    my_keyboard.press(Key.f5)
+    pyautogui.PAUSE = 1
     pyautogui.click()
     pyautogui.click()
 
